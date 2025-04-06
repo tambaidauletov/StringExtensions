28bc6f21aa191ae56c75241cab33ae59bdb4641b
Tamirlan <tbaidaul@my.centennialcollege.ca>
2025-04-06 18:53:31 -0400

    Add project files.

---
+++ StringExtensions/StringExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿namespace StringExtensions
+{
+    public static class StringExtensionMethods
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrEmpty(str)) return false;
+            return char.IsUpper(str[0]);
+        }
+    }
+}
